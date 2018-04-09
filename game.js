65ab7fa3da92172da4cb67a87476861097066a81
Michael Gerov <mhgerov@gmail.com>
2018-04-09 15:33:50 -0400

    prettify

--- game.js
+++ game.js
@@ -15,20 +15,42 @@ function Game() {
 		 *		Any region is fine if lastMove pointed to finished board -> this.board[lastmove].winner
 		 *	Local board is not won -> !this.board[global].winner
 		 */
-		if (!this.board.winner &&
-			this.board[global][local] == '' &&
-			(global == this.lastMove || this.board[lastMove].winner) &&
-			!this.board[global].winner) {
+		if (!this.board.winner && this.board[global][local] == '' && (global == this.lastMove || this.board[lastMove].winner) && !this.board[global].winner) {
 			//Mark board
 			this.board[global][local] = this.turn;
 			//Change turn
 			this.turn == 'X' ? this.turn = 'O' : this.turn = 'X';
 			this.lastMove = local
-		} else {console.log('Not a legal move')}
-		//check for winner locally
-		var lrow = local[1];
-		var lcol = local[3];
-		//globally
+			/* check for winner locally
+			 * check column
+			 * check diagonal
+			 * check all spots full -> tie
+			 */
+			if (
+				(this.board[global]['r'+local[1]+'c1'] == this.board[global]['r'+local[1]+'c2'] && this.board[global]['r'+local[1]+'c2'] == this.board[global]['r'+local[1]+'c3']) ||
+				(this.board[global]['r1c'+local[3]] == this.board[global]['r2c'+local[3]] && this.board[global]['r2c'+local[3]] == this.board[global]['r3c'+local[3]]) ||
+				(this.board[global].r1c1 == this.board[global].r2c2 && this.board[global].r2c2 == this.board[global].r3c3) || 
+				(this.board[global].r1c3 == this.board[global].r2c2 && this.board[global].r2c2 == this.board[global].r3c1)
+			) this.board[global].winner = this.board[global][local];
+			//check for local draw
+			//board full AND no winner
+			if (!this.board[global].winner) {
+				var full = true;
+				for (var r = 1; r<=3; r++) {
+					for (var c = 1; c<=3; c++) {
+						if (this.board[global]['r'+r+'c'+c] == '') {
+							full = false;
+							break;
+						}
+					}
+					if (!full) break;
+				}
+				if (full) this.board[global].winner = 'tie';
+			}
+			//check for winner globally
+		} else {
+			console.log('Not a legal move')
+		}
 	}
 
 	//helper functions
